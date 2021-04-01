62a9a8dd7528bec35557d57b7f7d25ddc6cece49
Florian Heuer <florian.heuer89@gmail.com>
2021-04-01 23:00:30 +0200

    initial commit

---
+++ app/src/main/java/de/alfiron/treegenerator/FirstOpenGLProjectRenderer.java
@@ -0,0 +1,28 @@
+package de.alfiron.treegenerator;
+
+import android.opengl.GLSurfaceView;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+class FirstOpenGLProjectRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
---
+++ app/src/main/java/de/alfiron/treegenerator/OpenGLActivity.java
@@ -0,0 +1,74 @@
+package de.alfiron.treegenerator;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OpenGLActivity extends Activity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        glSurfaceView = new GLSurfaceView(this);
+        setContentView(glSurfaceView);
+
+        final ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        final ConfigurationInfo configurationInfo =
+                activityManager.getDeviceConfigurationInfo();
+
+        final boolean supportsEs2 =
+                configurationInfo.reqGlEsVersion >= 0x20000
+                        || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                        && (Build.FINGERPRINT.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("unknown")
+                        || Build.MODEL.contains("google_sdk")
+                        || Build.MODEL.contains("Emulator")
+                        || Build.MODEL.contains("Android SDK built for x86")));
+
+        if (supportsEs2) {
+            // Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+
+            //glSurfaceView.setEGLConfigChooser(8,8,8,8,16,0);
+            // Assign our renderer.
+            glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, getString(R.string.do_not_support_message),
+                    Toast.LENGTH_LONG).show();
+
+            return;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (rendererSet) {
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(rendererSet){
+            glSurfaceView.onResume();
+        }
+    }
+}
